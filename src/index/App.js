b97bc3fbe43865b168d597f892513a3a0cf5b85a
logicwu0 <605524858@qq.com>
2018-03-23 17:59:34 +0800

    mod

--- src/index/App.js
+++ src/index/App.js
@@ -6,14 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hello index</p>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">与世界分享你的知识，经验和见解</h1>
+            </header>
+            <form action="">
+                <input type="text" placeholder="手机号或邮箱"/>
+                <input type="password" placeholder="密码"/>
+            </form>
+            <p>hello index</p>
       </div>
     );
   }
